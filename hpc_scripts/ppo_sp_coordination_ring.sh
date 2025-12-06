c440fda9273f260a1b735a71fafaba63bf3587b6
clutchnoob <67919245+clutchnoob@users.noreply.github.com>
2025-12-06 11:53:23 -0500

    sesame street

--- hpc_scripts/ppo_sp_coordination_ring.sh
+++ hpc_scripts/ppo_sp_coordination_ring.sh
@@ -9,11 +9,11 @@
 source /om2/user/mabdel03/anaconda/etc/profile.d/conda.sh
 conda activate /om/scratch/Mon/mabdel03/conda_envs/MAL_env
 
-# Navigate to project directory
-cd "$(dirname "$0")/.."
+# Navigate to project directory (SLURM_SUBMIT_DIR is where sbatch was called)
+cd "$SLURM_SUBMIT_DIR/.."
 cd src/human_aware_rl
 
-mkdir -p ../../hpc_scripts/logs
+mkdir -p "$SLURM_SUBMIT_DIR/logs"
 
 python -m human_aware_rl.ppo.train_ppo_sp --layout coordination_ring --seed 0 --fast
 
